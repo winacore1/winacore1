362928a6762921bb43f0050631ae18d7beba3bfd
Pradesh Kumar <winacore1@gmail.com>
2020-01-01 19:14:34 +0530

    Changes

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -100,7 +100,7 @@ jQuery(document).ready(function($) {
       data: str,
       success: function(msg) {
         // alert(msg);
-        if (msg == 'success') {
+        if (msg && msg.trim() == 'success') {
           $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
           $('.contactForm').find("input, textarea").val("");
